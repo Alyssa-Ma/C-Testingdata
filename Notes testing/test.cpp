d128785b29dc052c7efec371d0e6ed2e2682fc64
johnn <johnn@146.95.77.71>
2018-03-17 13:02:10 -0400

    Initial commit

---
+++ Notes testing/test.cpp
@@ -0,0 +1,19 @@
+/**
+ * Notes Case study
+ * dimensions, cost, price, profit
+ * 3/17/18
+ */
+#include "test.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double length, width, height;
+	double volume = length * width * height;
+
+	cout<<"enter dimensions";
+	cin>>length;
+
+}
+
---
+++ Notes testing/test.h
@@ -0,0 +1,17 @@
+/*
+ * test.h
+ *
+ *  Created on: Mar 17, 2018
+ *      Author: johnn
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+#endif /* TEST_H_ */
